192cb2d3e3dd6bb2e0a9cee77fc99ad4189c90fb
Christian Salazar <calaca89@gmail.com>
2019-11-28 15:44:36 -0500

    Added comments model and linter files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   belongs_to :user
-
-  validates :title, presence: true 
+  has_many :comments
+  validates :title, presence: true
 end
